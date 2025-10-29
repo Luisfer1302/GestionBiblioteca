c27729c62eee8b7741bd7562f8f250218c054fe0
Luiisfer <luiisfdrlv@gmail.com>
2025-10-29 02:19:32 +0100

    Tutoría 21/10/2025

---
+++ src/main/java/biblioteca/simple/app/Main.java
@@ -0,0 +1,17 @@
+package biblioteca.simple.app;
+
+public class Main {
+
+    // Crear objeto catálogo.
+    // Lista usuario.
+
+    //Carga de 4 libros + 4 PELÍCULAS
+    //Carga de 4 usuarios.
+
+    //MENU(1. LISTAR, 2. BUSCAR POR TÍTULO, 3. BUSCAR POR AÑO,
+    //4. PRESTAR, ¿Qué producto? ¿Qué usuario?
+    //5. DEVOLVER (OTRO USUARIO PUEDE DEVOLVER EL PRODUCTO, SÓLO SE MARCA COMO DEVUELTO Y YA ESTA)
+    //6. SALIR
+
+
+}
--- src/main/java/biblioteca/simple/servicios/Catalago.java
+++ src/main/java/biblioteca/simple/servicios/Catalago.java
@@ -10,8 +10,25 @@ public class Catalago {
     private final List<Producto> productos = new ArrayList<>();
 
     public void alta(Producto p){
+
         productos.add(p);
     }
 
-    public List<Producto> listar(){return new List}
+    public List<Producto> listar(){return new ArrayList<>(productos);}
+
+    public List<Producto> buscar(String titulo){
+        List<Producto> res = new ArrayList<>();
+        for (Producto p : productos){
+            if (p.getTitulo().toLowerCase().contains(titulo.toLowerCase())) res.add(p);
+        }
+        return res;
+    }
+
+    public List<Producto> buscar(int anho){
+        List<Producto> res = new ArrayList<>();
+        for (Producto p : productos){
+            if ( Integer.parseInt(p.getAnho()) == anho) res.add(p);
+        }
+        return res;
+    }
 }
